cb21af851b2662e59aa698e7f652c0a5b26a4375
mhieu2519 <nguyenminhhieu2000hd@gmail.com>
2025-08-11 02:23:03 +0700

    editFooter

--- blog-frontend/app/components/Footer.tsx
+++ blog-frontend/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="bg-pink-200 text-center p-4 text-sm text-gray-600">
-            &copy; {new Date().getFullYear()} Blog Chill. All rights reserved.
+            &copy; 2022- {new Date().getFullYear()} Lặng. All rights reserved.
         </footer>
     );
 }
